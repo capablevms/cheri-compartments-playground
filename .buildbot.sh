7a729ea1352207460cbb8ba72dc0030794ace57d
Gabi Moldovan <gabi_250@live.com>
2022-12-07 15:27:05 +0000

    Configure buildbot and bors.

---
+++ .buildbot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2020-2022 The CapableVMs "CHERI Examples" Contributors.
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+set -eou pipefail
+
+echo "Checking that all the purecap examples build for all platforms..."
+
+export SSHPORT=10022
+
+args=(
+    --architecture morello-hybrid
+    # Qemu System to use
+    --qemu-cmd $CHERI_DIR/sdk/bin/qemu-system-morello
+    --bios edk2-aarch64-code.fd
+    --disk-image $CHERI_DIR/cheribsd-morello-hybrid.img
+    # Required build-dir in CheriBSD
+    --build-dir .
+    --ssh-port $SSHPORT
+    --ssh-key $HOME/.ssh/id_ed25519.pub
+)
+
+BUILDBOT_PLATFORM=morello-hybrid python3 .buildbot-run-tests.py "${args[@]}"
